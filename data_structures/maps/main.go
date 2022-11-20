d3a84d51a6608faf29e1e45f89e2ec44eb1f8b2b
Martin Kutlak <mkutlak@users.noreply.github.com>
2022-11-20 22:20:54 +0100

    Final Maps

--- data_structures/maps/main.go
+++ data_structures/maps/main.go
@@ -6,8 +6,24 @@ func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4b7f6c",
+		"white": "#ffffff",
 	}
 
 	fmt.Println(colors)
 
+	weird := make(map[string]string)
+
+	weird["name"] = "typist"
+	weird["surname"] = "normist"
+	delete(weird, "name")
+
+	fmt.Println(weird)
+
+	printMap(colors)
+}
+
+func printMap(m map[string]string) {
+	for color, hex := range m {
+		fmt.Printf("color:%s - hex:%s\n", color, hex)
+	}
 }
