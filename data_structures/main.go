c01bce16ca752b2e0fa98c1990befc2dc15a4bf2
Martin Kutlak <mkutlak@users.noreply.github.com>
2022-11-20 21:23:53 +0100

    Receive structs

--- data_structures/main.go
+++ data_structures/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
@@ -26,4 +32,24 @@ func main() {
 
 	fmt.Printf("%v %v\n", jim.firstName, jim.lastName)
 
+	jim.contact = contactInfo{
+		email:   "jim@jones.com",
+		zipCode: 90011,
+	}
+
+	fmt.Println(jim)
+
+	jim.print()
+
+	jim.updateName("Jimmy")
+	jim.print()
+
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
 }
