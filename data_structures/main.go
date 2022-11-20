5dccf510b13a7dde378582cb6f6f0c300b95613d
Martin Kutlak <mkutlak@users.noreply.github.com>
2022-11-20 21:13:27 +0100

    Struct

--- data_structures/main.go
+++ data_structures/main.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
 func main() {
+
+	alex := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+	}
+
+	fmt.Printf("%s %s\n", alex.firstName, alex.lastName)
+
+	// Declare variable jim of type person
+	var jim person
+
+	fmt.Printf("%v %v\n", jim.firstName, jim.lastName)
+
+	jim.firstName = "Jim"
+	jim.lastName = "Jones"
+
+	fmt.Printf("%v %v\n", jim.firstName, jim.lastName)
 
 }
