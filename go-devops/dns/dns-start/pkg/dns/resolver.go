cad5f8f0c716e5053109c8775d293708d45b6ec4
Martin Kutlak <mkutlak@users.noreply.github.com>
2023-01-05 22:39:52 +0100

    add dns

---
+++ go-devops/dns/dns-start/pkg/dns/resolver.go
@@ -0,0 +1,86 @@
+package dns
+
+import (
+	"bufio"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"net"
+
+	"golang.org/x/net/dns/dnsmessage"
+)
+
+const ROOT_SERVERS = "198.41.0.4,199.9.14.201,192.33.4.12,199.7.91.13,192.203.230.10,192.5.5.241,192.112.36.4,198.97.190.53"
+
+func handlePacket(pc net.PacketConn, addr net.Addr, buf []byte) error {
+	return fmt.Errorf("not implemented yet")
+}
+
+func outgoingDnsQuery(servers []net.IP, question dnsmessage.Question) (*dnsmessage.Parser, *dnsmessage.Header, error) {
+	max := ^uint16(0)
+	randomNumber, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	message := dnsmessage.Message{
+		Header: dnsmessage.Header{
+			ID:       uint16(randomNumber.Int64()),
+			Response: false,
+			OpCode:   dnsmessage.OpCode(0),
+		},
+		Questions: []dnsmessage.Question{question},
+	}
+
+	buf, err := message.Pack()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var conn net.Conn
+	for _, server := range servers {
+		conn, err = net.Dial("udp", server.String()+":53")
+		if err == nil {
+			break
+		}
+	}
+	if conn == nil {
+		return nil, nil, fmt.Errorf("failed to make connection to server: %s", err)
+	}
+
+	_, err = conn.Write(buf)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	answer := make([]byte, 512)
+	n, err := bufio.NewReader(conn).Read(answer)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	conn.Close()
+
+	var p dnsmessage.Parser
+
+	header, err := p.Start(answer[:n])
+	if err != nil {
+		return nil, nil, fmt.Errorf("parser start error: %s", err)
+	}
+
+	questions, err := p.AllQuestions()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if len(questions) != len(message.Questions) {
+		return nil, nil, fmt.Errorf("answer packet does not have the same amount of questions")
+	}
+
+	err = p.SkipAllQuestions()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &p, &header, nil
+}
